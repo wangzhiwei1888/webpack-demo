053acf0fc32d22a2dec9594c96a5ca29438660b8
wangzhiwei8 <wangzhiwei8@jd.com>
2018-06-22 14:03:25 +0800

    add 公共路径

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,19 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const webpack = require('webpack');
 
+const ASSET_PATH = process.env.ASSET_PATH || './';
+// const ASSET_PATH = process.env.ASSET_PATH || 'http://www.baidu.com/';
+
 module.exports = {
 
 	entry: {
+		
 		another:'./src/another-module.js',
-		app:'./src/index.js'
+		app:'./src/index.js',
+		vendor:[
+			'lodash'
+		]
+
 		
 	},
 	// devtool: 'inline-source-map',
@@ -30,7 +38,7 @@ module.exports = {
 		}]
 	},
 	output:{
-
+		publicPath:ASSET_PATH,
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname,'dist')
 	},
@@ -40,9 +48,12 @@ module.exports = {
 			title:'Output Management'
 		}),
 		new webpack.optimize.CommonsChunkPlugin({
-			name: 'common' // Specify the common bundle's name.
+			name: 'vendor' // Specify the common bundle's name.
 		}),
-		new ExtractTextPlugin("style.css")
+		new ExtractTextPlugin("style.css"),
+		new webpack.DefinePlugin({
+	      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+	    })
 	]
 
 
