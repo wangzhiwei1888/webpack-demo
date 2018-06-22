9afafd956fdfc343b9fb9fa81e3b934cf5642d79
wangzhiwei8 <wangzhiwei8@jd.com>
2018-06-22 10:31:59 +0800

    动态生成 dist 目录

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
 
 	entry: {
-		app:'./src/index.js',
-		print:'./src/print.js'
+		print:'./src/print.js',
+		app:'./src/index.js'
+		
 	},
 	output:{
 
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname,'dist')
-	}
+	},
+	plugins:[
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title:'Output Management'
+		})
+	]
 
 
 }
