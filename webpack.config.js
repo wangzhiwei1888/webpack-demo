582f3b35057306bd08ccdecbc7f4e0e0cde85e81
wangzhiwei8 <wangzhiwei8@jd.com>
2018-06-22 11:54:26 +0800

    代码拆分 防止重复 css 抽出，公用部分抽出

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const webpack = require('webpack');
 
 module.exports = {
 
 	entry: {
-		print:'./src/print.js',
+		another:'./src/another-module.js',
 		app:'./src/index.js'
 		
 	},
@@ -18,9 +20,13 @@ module.exports = {
 	module:{
 		rules: [{
 			test: /\.css$/,
-			use: [
-				'style-loader', 'css-loader'
-			]
+			// use: [
+			// 	'style-loader', 'css-loader'
+			// ]
+			use: ExtractTextPlugin.extract({
+	          fallback: "style-loader",
+	          use: "css-loader"
+	        })
 		}]
 	},
 	output:{
@@ -32,7 +38,11 @@ module.exports = {
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title:'Output Management'
-		})
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common' // Specify the common bundle's name.
+		}),
+		new ExtractTextPlugin("style.css")
 	]
 
 
